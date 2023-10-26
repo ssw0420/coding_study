1dceebb946798aec2dd361035031be64666e337f
ssw0420 <tmddn5645@gmail.com>
2023-10-26 14:16:31 +0900

    update
    update

---
+++ programmers/Sticker_Collect/MainTest/Solution.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+class Solution
+{
+    public int solution(int[] sticker)
+    {
+        int answer = 0;
+        Array.Fill(sticker, 0);
+
+        // System.Console.WriteLine(sticker[0]);
+
+        return answer;
+    }
+    
+    public static void Main()
+    {
+        int[] sticker = new int[100000]; 
+        Solution solution = new Solution();
+        solution.solution(sticker);
+    }
+}
--- programmers/Sticker_Collect/Sticker_Collect/Solution.cs
+++ programmers/Sticker_Collect/Sticker_Collect/Solution.cs
@@ -1,2 +1,14 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+class Solution
+{
+    private int[] sticker = new int[100000];
+    public int solution(int[] sticker)
+    {
+        int answer = 0;
+
+        System.Console.WriteLine("Hello C#");
+
+        return answer;
+    }
+}
