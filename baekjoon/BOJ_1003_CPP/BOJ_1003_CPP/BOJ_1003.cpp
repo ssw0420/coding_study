d19827d093932f3f0e67de697a0486ec24a69b2d
tmddn5645 <tmddn5645@gmail.com>
2025-11-03 15:35:33 +0900

    BOJ_1003_CPP
    BOJ_1003_CPP
    피보나치 함수
    - DP

---
+++ baekjoon/BOJ_1003_CPP/BOJ_1003_CPP/BOJ_1003.cpp
@@ -0,0 +1,55 @@
+﻿#include <iostream>
+
+using namespace std;
+
+//static int zero;
+//static int one;
+//
+//int fibonacci(int n)
+//{
+//    if (n == 0)
+//    {
+//        zero++;
+//        return 0;
+//    }
+//    else if (n == 1)
+//    {
+//        one++;
+//        return 1;
+//    }
+//    else
+//    {
+//        return fibonacci(n-1) + fibonacci(n-2);
+//    }
+//}
+
+int main(void)
+{
+	ios::sync_with_stdio(false);
+	cin.tie(nullptr);
+
+    int T;
+    cin >> T;
+
+    int zero[41];
+    int one[41];
+
+    zero[0] = 1; one[0] = 0;
+    zero[1] = 0; one[1] = 1;
+
+    for (int i = 2; i <= 40; i++)
+    {
+        zero[i] = zero[i - 1] + zero[i - 2];
+        one[i] = one[i - 1] + one[i - 2];
+    }
+
+    while (T--)
+    {
+        int N;
+        cin >> N;
+        cout << zero[N] << " " << one[N] << "\n";
+    }
+
+    
+	return 0;
+}
