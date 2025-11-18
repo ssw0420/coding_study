a861be0b00b5ce3c18110bdeed537472ab54eff6
tmddn5645 <tmddn5645@gmail.com>
2025-11-19 03:48:45 +0900

    BOJ_1927_CPP
    BOJ_1927_CPP
    최소힙

---
+++ baekjoon/BOJ_1927_CPP/BOJ_1927_CPP/BOJ_1927.cpp
@@ -0,0 +1,40 @@
+#include <iostream>
+#include <vector>
+#include <queue>
+
+using namespace std;
+
+int main(void)
+{
+	ios::sync_with_stdio(false);
+	cin.tie(nullptr);
+
+    priority_queue<int, vector<int>, greater<int>> pq;
+
+    int N;
+    cin >> N;
+
+    while (N--)
+    {
+        int x;
+        cin >> x;
+
+        if (x == 0)
+        {
+            if (pq.empty())
+            {
+                cout << 0 << "\n";
+            }
+            else
+            {
+                cout << pq.top() << "\n";
+                pq.pop();
+            }
+        }
+        else {
+            pq.push(x);
+        }
+    }
+
+	return 0;
+}
