bd7364205d756d2ad2e6241c52a5dfd317d4c4fc
tmddn5645 <tmddn5645@gmail.com>
2025-11-28 01:39:10 +0900

    BOJ_11727_CPP
    BOJ_11727_CPP
    2xn 타일링 2

---
+++ baekjoon/BOJ_11727_CPP/BOJ_11727_CPP/BOJ_11727.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+int main(void)
+{
+	ios::sync_with_stdio(false);
+	cin.tie(nullptr);
+
+	int n;
+	cin >> n;
+
+	vector<int> dp(n + 1, 0);
+
+	dp[1] = 1;
+	if (n >= 2)
+	{
+		dp[2] = 3;
+	}
+
+	for (int i = 3; i <= n; i++)
+	{
+		dp[i] = (dp[i - 1] + 2 * dp[i - 2]) % 10007;
+	}
+
+	cout << dp[n];
+
+	return 0;
+}
