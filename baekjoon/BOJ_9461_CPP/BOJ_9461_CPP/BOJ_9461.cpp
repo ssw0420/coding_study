16036a4fc55a8924d104bf741527577f47a8f693
tmddn5645 <tmddn5645@gmail.com>
2025-11-29 04:06:43 +0900

    BOJ_9461_CPP
    BOJ_9461_CPP
    파도반 수열

---
+++ baekjoon/BOJ_9461_CPP/BOJ_9461_CPP/BOJ_9461.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+int main(void)
+{
+	ios::sync_with_stdio(false);
+	cin.tie(nullptr);
+
+	int T;
+	cin >> T;
+
+	vector <long long> dp(101, 1);
+	dp[1] = 1;
+	dp[2] = 1;
+	dp[3] = 1;
+
+	for (int i = 4; i <= 100; i++)
+	{
+		dp[i] = dp[i - 2] + dp[i - 3];
+	}
+
+
+	for (int i = 0; i < T; i++)
+	{
+		int N;
+		cin >> N;
+
+		cout << dp[N] << "\n";
+	}
+
+	return 0;
+}
